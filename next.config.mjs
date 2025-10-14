223deca5f902b1fcdbe2fa1a4c697beac01b511b
mohammadreza <mohammadrezabitarfan7@gmail.com>
2025-10-14 11:12:56 +0330

    remove redirects

--- next.config.mjs
+++ next.config.mjs
@@ -15,35 +15,35 @@ const nextConfig = {
     ],
   },
 
-  async redirects() {
-    return [
-      {
-        source: "/appreciation",
-        destination: "/yearbook",
-        permanent: true, // 301
-      },
-      {
-        source: "/price-list",
-        destination: "/",
-        permanent: true, // 301
-      },
-      {
-        source: "/component/:path*",
-        destination: "/",
-        permanent: true, // 301
-      },
-      {
-        source: "/images/:path*",
-        destination: "https://media.apadanacalendar.com/images/:path*",
-        permanent: true,
-      },
-      {
-        source: "/templates/:path*",
-        destination: "https://media.apadanacalendar.com/templates/:path*",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/appreciation",
+  //       destination: "/yearbook",
+  //       permanent: true, // 301
+  //     },
+  //     {
+  //       source: "/price-list",
+  //       destination: "/",
+  //       permanent: true, // 301
+  //     },
+  //     {
+  //       source: "/component/:path*",
+  //       destination: "/",
+  //       permanent: true, // 301
+  //     },
+  //     {
+  //       source: "/images/:path*",
+  //       destination: "https://media.apadanacalendar.com/images/:path*",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/templates/:path*",
+  //       destination: "https://media.apadanacalendar.com/templates/:path*",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
